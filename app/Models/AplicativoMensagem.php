cd9dd5c1e47976bcd08ec24294e37eefef0e3bb7
Rafael do Nascimento <rafaelnlima@live.com>
2022-03-17 09:50:32 -0300

    criado model cliente, contato

---
+++ app/Models/AplicativoMensagem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AplicativoMensagem extends Model
+{
+    use HasFactory;
+}
