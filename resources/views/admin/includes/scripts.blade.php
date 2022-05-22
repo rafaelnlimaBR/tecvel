7976e5cb983ce0abbb48accf0f8579b9e774693f
Rafael do Nascimento <rafaelnlima@live.com>
2022-05-22 19:10:09 -0300

    "ordem datatabe"

--- resources/views/admin/includes/scripts.blade.php
+++ resources/views/admin/includes/scripts.blade.php
@@ -370,6 +370,7 @@
     });
 
     $('#tabela-contratos').dataTable({
+        "order": [ 0, 'DESC' ],
         "language":{
             "url":"//cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json"
         },
